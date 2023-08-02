6e14df3fe69a1b91ce66feb9451513ba62c99020
Di0n-0 <di0n-0@protonmail.com>
2023-08-02 23:01:14 +0300

    Added menu, programme can now be operated by the user completly

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 ### The following hand-detection code is by cvzone(https://www.computervision.zone/) the video it is taken from: https://www.youtube.com/watch?v=RQ-2JWzNc6k
+import sys
 import cv2
 import math
 import time
 import numpy as np
 import mediapipe as mp
 from ultralytics import YOLO
+import PySimpleGUI as sg
 
 class HandDetector:
     """
@@ -203,6 +205,71 @@ def object_segment(img, model):
 
     return mask_rgba, center, max_side_length
 
+def draw_pre_recorded(img, landmark_list):
+    line_list = [(0, 1), (1, 2), (2, 3), (3, 4), (0, 5), (5, 6), (6, 7), (7, 8), (5, 9), (9, 10), (10, 11), (11, 12), (9, 13), (13, 14), (14, 15), (15, 16), (13, 17), (0, 17), (17, 18), (18, 19), (19, 20)]
+    for line in line_list:
+        if len(landmark_list) >= 21:
+            cv2.line(img, (landmark_list[line[0]][0], landmark_list[line[0]][1]), (landmark_list[line[1]][0], landmark_list[line[1]][1]), (255, 255, 255), thickness=2)
+        if len(landmark_list) == 42:
+            cv2.line(img, (landmark_list[line[0] + 21][0], landmark_list[line[0] + 21][1]), (landmark_list[line[1] + 21][0], landmark_list[line[1] + 21][1]), (255, 255, 255), thickness=2)
+    for landmark in landmark_list:
+        cv2.circle(img, (landmark[0], landmark[1]), radius=5, color=(186, 85, 211), thickness=-2)
+    return img
+
+def gui_init():
+    global video, pre_recorded_file, mirror_effect, tutor, record, slowing_factor, exit
+    layout_gui = [
+        [sg.Text("Mirror Effect"), sg.Checkbox("", default=mirror_effect, key="mirror_effect", enable_events=True)],
+        [sg.Text("Record"), sg.Checkbox("", default=record, key="record", enable_events=True)],
+        [sg.Text("Tutor"), sg.Checkbox("", default=tutor, key="tutor", enable_events=True)],
+        [sg.Text("Slowing Factor", key="slowing_factor_text"), sg.Slider(range=(1, 10), default_value=slowing_factor, orientation="h", key="slowing_factor", enable_events=True)],
+        [sg.Text("Pre-recorded File", key="pre_recorded_file", visible=tutor), sg.InputText(default_text=pre_recorded_file, key="pre_recorded_file_input_box", visible=tutor), sg.FileBrowse(visible=tutor, key="pre_recorded_file_browse", enable_events=True)],
+        [sg.Text("Video", key="video_file", visible=record), sg.InputText(default_text=video, key="video_file_input_box", visible=record), sg.FileBrowse(visible=record, key="video_file_browse", enable_events=True)],
+        [sg.Button("Save", key="save", enable_events=True), sg.Button("Exit ARpeggio", key="exit", enable_events=True)]
+    ]
+    sg.theme("LightGrey5")
+    window_gui = sg.Window("Settings", layout_gui)
+
+    while True:
+        event, values = window_gui.read()
+        if event == sg.WINDOW_CLOSED:
+            break
+        if event is not None:
+            if event == "exit":
+                exit = True
+                break
+
+            mirror_effect = values["mirror_effect"]
+            tutor = values["tutor"]
+            record = values["record"]
+            slowing_factor = values["slowing_factor"]
+            
+            window_gui["slowing_factor_text"].update(visible=tutor and not record)
+            window_gui["slowing_factor"].update(visible=tutor and not record)
+            window_gui["pre_recorded_file"].update(visible=tutor and not record)
+            window_gui["pre_recorded_file"].update(visible=tutor and not record)
+            window_gui["video_file"].update(visible=record and not tutor)
+            window_gui["pre_recorded_file_input_box"].update(visible=tutor and not record)
+            window_gui["video_file_input_box"].update(visible=record and not tutor)
+            window_gui["pre_recorded_file_browse"].update(visible=tutor and not record)
+            window_gui["video_file_browse"].update(visible=record and not tutor)
+
+            if event == "save":
+                if tutor and not record:
+                    pre_recorded_file = values["pre_recorded_file_input_box"]
+                elif record and not tutor:
+                    video = values["video_file_input_box"]
+
+    window_gui.close()
+
+def handle_tweaks():
+    gui_init()
+    if tutor:
+        global pre_recorded_file, pre_recorded
+        with open(pre_recorded_file, 'r') as text_file:
+            data = text_file.read().rstrip(',')
+            pre_recorded = [float(x) for x in data.split(',')]
+
 model_guitar_detect = YOLO("guitar_detect.pt")
 
 model_fretboard_detect = YOLO("fretboard_detect.pt")
@@ -215,12 +282,10 @@ detector = HandDetector(detectionCon=0.8, maxHands=2)
 
 img_cut_value = 0
 
-#cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture('test3.mp4')
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-success, img = cap.read()
-h, w, _ = img.shape
+video = ""
+
+pre_recorded_file = ""
+pre_recorded = []
 
 mirror_effect = False
 record = False
@@ -230,20 +295,37 @@ counter = 0
 step = 127
 slowing_factor = 1
 
-if tutor:
-    with open("output.txt", 'r') as text_file:
-        data = text_file.read().rstrip(',')
-        pre_recorded = [float(x) for x in data.split(',')]
-    
+exit = False
+
+handle_tweaks()
+
+if record:
+    try:
+        cap = cv2.VideoCapture(video)
+    except FileNotFoundError:
+        print("There is no such video file, exiting")
+        sys.exit()
+else:
+    cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+success, img = cap.read()
+h, w, _ = img.shape
+
+previousTime = 0
+currentTime = 0
 
 while True:
     success, img = cap.read()
 
-    if success is False:
+    if success is False or exit:
         break
 
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-    print(fps)
+    currentTime = time.time()
+    fps = 1 / (currentTime-previousTime)
+    previousTime = currentTime
+     
+    cv2.putText(img, str(int(fps))+" FPS", (10, 70), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
 
     img_hands = img.copy()
     
@@ -255,7 +337,7 @@ while True:
             img_fretboard, center, max_side_length = object_segment(img_fretboard, model_fretboard_seg)
             center[0] += sub_window_coord[0][0] + sub_window_coord[1][0]
             center[1] += sub_window_coord[0][1] + sub_window_coord[1][1]
-            if tutor:
+            if tutor and max_side_length != 0:
                 sub_list = pre_recorded[counter : min(counter + step, len(pre_recorded))]
                 landmark_list = []
                 landmarks = list(sub_list[:-1])
@@ -264,6 +346,7 @@ while True:
                 except IndexError:
                     cap.release()
                     cv2.destroyAllWindows() 
+                    sys.exit()
 
                 for i in range(0, len(landmarks), 3):
                     r, theta, z = sub_list[i:i+3]
@@ -274,8 +357,7 @@ while True:
                     y = int(r * np.sin(theta) + center[1])
 
                     landmark_list.append([int(x), int(y), int(z)])
-                for landmark in landmark_list:
-                    cv2.circle(img, (landmark[0], landmark[1]), radius=5, color=(0, 0, 255), thickness=-2)
+                img = draw_pre_recorded(img, landmark_list)
                 if int(time.time()) % slowing_factor == 0:
                     counter += step
 
@@ -292,7 +374,7 @@ while True:
                     theta = np.arctan2(landmark[1] - center[1], landmark[0] - center[0])
                     write_data.extend([r, theta, landmark[2]])
             write_data.extend([max_side_length])
-            with open("output.txt", 'a') as text_file:
+            with open(video.replace(".mp4", ".txt"), 'a') as text_file:
                 text_file.write(','.join(str(x) for x in write_data))
                 text_file.write(',')
 
@@ -309,7 +391,7 @@ while True:
         cv2.imshow("img_fretboard", img_fretboard)
     
     if cv2.waitKey(1) == 27:
-        break
+        handle_tweaks()
 
 
 cap.release()
