2fabc808be0bb14d19887b8f106a65aad87015a5
di0n-0 <Di0n-0>
2024-05-03 22:19:38 +0300

    changed video source to 0

--- src/main.py
+++ src/main.py
@@ -174,7 +174,7 @@ def main():
     handle_tweaks()
 
    
-    cap = cv2.VideoCapture("../test.mp4")#0
+    cap = cv2.VideoCapture(0)#0
     
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
